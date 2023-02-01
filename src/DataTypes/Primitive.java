d257cedfa6431be0a618b2b08fd54a7b1661d65d
Siruu123 <112840838+Siruu123@users.noreply.github.com>
2023-02-01 10:58:16 +0530

    Data types

---
+++ src/DataTypes/Primitive.java
@@ -0,0 +1,34 @@
+package DataTypes;
+
+public class Primitive {
+	public static void main(String args[]) {
+		//taking integer values
+		int i = 10;
+		int b = 20;
+		int addition = 10+20;
+		int subraction = 10-20;
+		int newInteger = 30;
+		
+		System.out.println(newInteger/i);
+		System.out.println(newInteger/b);
+		System.out.println("Add="+addition);
+		System.out.println("Sub="+subraction);
+		
+		char oneCharacter = 'c';
+		boolean aBoolean = true;
+		float aFloat = 3.11f;
+		
+		System.out.println("char = "+oneCharacter);
+		System.out.println("boolean="+aBoolean);
+		System.out.println("float="+aFloat);
+		
+		
+
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/DataTypes/PrimitiveDataType.java
@@ -0,0 +1,35 @@
+package DataTypes;
+
+public class PrimitiveDataType {
+	public static void main (String args[]) {
+		int one = 1;
+		int five = 5;
+		
+		int addition = 1+5;
+		int subraction = 1-5;
+		float newInteger = 34;
+		byte b = 100;
+		short s = 200;
+		long l = 65;
+		
+		System.out.println(newInteger/five);
+		System.out.println(newInteger/one);
+		System.out.println("Add="+ addition);
+		System.out.println("Sub="+subraction);
+	
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(l);
+		
+		char oneCharacter = 'c';
+		boolean aBoolean = true;
+		System.out.println("char =" + oneCharacter);
+		System.out.println("boolean ="+ aBoolean);
+		
+		
+		
+		
+		
+	}
+
+}
